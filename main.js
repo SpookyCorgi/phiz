4e5a54e69b274e9f58005124d10d424725c39a52
James Lee <b05502067@ntu.edu.tw>
2022-04-11 02:09:45 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,335 @@
+import './style.css'
+
+import * as THREE from 'three'
+import { FBXLoader } from 'three/examples/jsm/loaders/FBXLoader';
+import { GLTFExporter } from 'three/examples/jsm/exporters/GLTFExporter.js';
+import modelFile from './assets/face.fbx?url'
+import {
+  ApplicationContext,
+  FacemojiAPI,
+  FaceTracker,
+  ResourceFileSystem,
+} from '@facemoji/mocap4face'
+import { Quaternion } from 'three';
+
+
+let canvas, clock
+let head, grpScale, eyeLeft, eyeRight, morphTargetDictionary, morphTargetInfluences
+let loaded = false
+let recording = false
+let recordedData = []
+let video = document.querySelector("#videoElement");
+let fps = 30;
+
+//three js init
+function init () {
+  clock = new THREE.Clock()
+
+  canvas = document.querySelector('#bg')
+  const scene = new THREE.Scene()
+  scene.background = new THREE.Color(0xffffff)
+
+  const camera = new THREE.PerspectiveCamera(45, 4 / 3, 0.1, 100);
+  camera.position.set(0, 0, 50)
+
+  const renderer = new THREE.WebGLRenderer({
+    canvas: canvas,
+    antialias: true
+  })
+  renderer.setPixelRatio(window.devicePixelRatio)
+  renderer.setSize(canvas.clientWidth, canvas.clientWidth / 4 * 3)
+
+  renderer.toneMapping = THREE.ACESFilmicToneMapping
+  renderer.outputEncoding = THREE.sRGBEncoding
+
+  const fbxLoader = new FBXLoader()
+  fbxLoader.load(modelFile,
+    (object) => {
+      grpScale = object
+      console.log(object)
+      head = object.getObjectByName('head')
+      eyeLeft = object.getObjectByName('eyeLeft')
+      eyeRight = object.getObjectByName('eyeRight')
+      for (let i = 0; i < 52; i++) {
+        head.morphTargetDictionary[i] = i
+      }
+      morphTargetDictionary = head.morphTargetDictionary
+      morphTargetInfluences = head.morphTargetInfluences
+      scene.add(object)
+      loaded = true
+    },
+    (xhr) => {
+      //console.log((xhr.loaded / xhr.total) * 100 + '% loaded')
+    },
+    (error) => {
+      console.log(error)
+    }
+  )
+
+  //light
+  const dirLight = new THREE.DirectionalLight(0xffffff)
+  dirLight.position.set(0, 100, 50)
+  dirLight.castShadow = true
+  scene.add(dirLight)
+
+  //animation loop
+  renderer.setAnimationLoop(() => {
+    renderer.render(scene, camera)
+  });
+
+  window.addEventListener('resize', () => {
+    camera.aspect = canvas.clientWidth / canvas.clientHeight
+    camera.updateProjectionMatrix()
+    renderer.setSize(canvas.clientWidth, canvas.clientWidth / 4 * 3)
+  });
+}
+
+function getWebcam () {
+  if (navigator.mediaDevices.getUserMedia) {
+    navigator.mediaDevices.getUserMedia({ video: true })
+      .then(function (stream) {
+        video.srcObject = stream;
+      })
+      .catch(function (err0r) {
+        console.log("Something went wrong!");
+      });
+  }
+}
+
+function tracking () {
+  const context = new ApplicationContext(window.location.href)
+  const fs = new ResourceFileSystem(context)
+  // Initialize the API and activate API key
+  // Note that without an API key the SDK works only for a short period of time
+  FacemojiAPI.initialize('dfe3er64ijyqmqtqg2nt6btjuus754busbnboca7ef553iw32qyf27i', context).then((activated) => {
+    if (activated) {
+      console.info('API successfully activated')
+    } else {
+      console.info('API could not be activated')
+    }
+  })
+
+  // Initialize
+  const asyncTracker = FaceTracker.createVideoTracker(fs)
+    .then((tracker) => {
+      console.log('Started tracking')
+      requestAnimationFrame(track)
+      return tracker
+    })
+    .logError('Could not start tracking')
+
+  /**
+  * Performs face tracking, called every animation frame.
+  */
+  function track () {
+    requestAnimationFrame(track);
+    var tracker = asyncTracker.currentValue;
+    // Track only when everything is fully loaded and video is running
+    if (!tracker || video === null) {
+      return;
+    }
+    // Face tracking
+    var lastResult = tracker.track(video);
+    if (lastResult == null) {
+      return; // No face found or video frame could not be processed
+    }
+    // Update 3D model
+    let blendshapes = {}
+    let rightBrowValue = 0
+    let leftBrowValue = 0
+    //rotation
+    const rotationBlendshapes = faceRotationToBlendshapes(lastResult.rotationQuaternion)
+    grpScale.setRotationFromEuler(new THREE.Euler(
+      rotationBlendshapes[3][1] - rotationBlendshapes[2][1],
+      rotationBlendshapes[0][1] - rotationBlendshapes[1][1],
+      rotationBlendshapes[5][1] - rotationBlendshapes[4][1], 'XYZ'))
+    eyeLeft.setRotationFromEuler(new THREE.Euler(
+      -rotationBlendshapes[3][1] + rotationBlendshapes[2][1],
+      -rotationBlendshapes[0][1] + rotationBlendshapes[1][1],
+      -rotationBlendshapes[5][1] + rotationBlendshapes[4][1], 'XYZ'))
+    eyeRight.setRotationFromEuler(new THREE.Euler(
+      -rotationBlendshapes[3][1] + rotationBlendshapes[2][1],
+      -rotationBlendshapes[0][1] + rotationBlendshapes[1][1],
+      -rotationBlendshapes[5][1] + rotationBlendshapes[4][1], 'XYZ'))
+    //affect morphtargetInfluence
+    for (const [name, value] of lastResult.blendshapes) {
+      if (name == 'browInnerUp_R') {
+        rightBrowValue = value
+      }
+      if (name == 'browInnerUp_L') {
+        leftBrowValue = value
+      }
+      if (morphTargetDictionary[name] != undefined) {
+        morphTargetInfluences[morphTargetDictionary[name]] = value
+        if (recording) {
+          blendshapes[name] = value
+        }
+      }
+    }
+    //eyebrow inner
+    morphTargetInfluences[morphTargetDictionary['browInnerUp']] = (rightBrowValue + leftBrowValue) / 2
+    if (recording) {
+      blendshapes['browInnerUp'] = (rightBrowValue + leftBrowValue) / 2
+    }
+
+    if (recording) {
+      let time = clock.getElapsedTime()
+      let rotation = new THREE.Euler(
+        rotationBlendshapes[3][1] - rotationBlendshapes[2][1],
+        rotationBlendshapes[0][1] - rotationBlendshapes[1][1],
+        rotationBlendshapes[5][1] - rotationBlendshapes[4][1], 'XYZ')
+      let quaternion = new THREE.Quaternion()
+      quaternion.setFromEuler(rotation)
+      let obj = {
+        time: time,
+        blendshapes: blendshapes,
+        rotation: quaternion
+      }
+      recordedData.push(obj)
+    }
+  }
+  /**
+  * Converts head rotation to blendshape-like values so that we can show it in the UI as well.
+  * @param rotation rotation from the tracker
+  * @returns rotation represented as 6 blendshapes
+  */
+  function faceRotationToBlendshapes (rotation) {
+    var euler = rotation.toEuler();
+    var halfPi = Math.PI * 0.5;
+    return [
+      ['headLeft', Math.max(0, euler.y) / halfPi],
+      ['headRight', -Math.min(0, euler.y) / halfPi],
+      ['headUp', -Math.min(0, euler.x) / halfPi],
+      ['headDown', Math.max(0, euler.x) / halfPi],
+      ['headRollLeft', -Math.min(0, euler.z) / halfPi],
+      ['headRollRight', Math.max(0, euler.z) / halfPi],
+    ];
+  }
+}
+
+function createAnimation () {
+  if (recordedData.length != 0) {
+    let animation = []
+    for (let i = 0; i < 52; i++) {
+      animation.push([])
+    }
+    let rotationAnimation = []
+    let time = []
+    let startTime = 0
+    let finishedFrames = 0
+    recordedData.forEach((d, i) => {
+      if (i == 0) {
+        //first frame
+        startTime = d.time
+        Object.entries(d.blendshapes).forEach(([key, value]) => {
+          animation[morphTargetDictionary[key]].push(value)
+        });
+        rotationAnimation.push(d.rotation)
+        time.push(finishedFrames / fps)
+        finishedFrames++
+      } else {
+        //interpolate frames
+        while (d.time > (finishedFrames / fps + startTime)) {
+          let timeBefore = ((finishedFrames / fps + startTime) - recordedData[i - 1].time)
+          let timeAfter = (d.time - (finishedFrames / fps + startTime))
+          Object.entries(d.blendshapes).forEach(([key, value]) => {
+            let valueAfter = value
+            let valueBefore = recordedData[i - 1].blendshapes[key]
+            animation[morphTargetDictionary[key]].push((valueBefore * timeAfter + valueAfter * timeBefore) / (timeBefore + timeAfter))
+          });
+          let quaternion = new Quaternion()
+          quaternion.slerpQuaternions(recordedData[i - 1].rotation, d.rotation, timeBefore / (timeBefore + timeAfter))
+          rotationAnimation.push(quaternion)
+          time.push(finishedFrames / fps)
+          finishedFrames++
+        }
+      }
+    })
+    let tracks = []
+    //create morph animation
+    Object.entries(recordedData[0].blendshapes).forEach(([key, value]) => {
+      let i = morphTargetDictionary[key]
+      let track = new THREE.NumberKeyframeTrack(`head.morphTargetInfluences[${i}]`, time, animation[i])
+      tracks.push(track)
+    });
+    //create rotation animation
+    let groupQuaternionFlat = []
+    let eyeQuaternionFlat = []
+    rotationAnimation.forEach(d => {
+      groupQuaternionFlat.push(d.x)
+      groupQuaternionFlat.push(d.y)
+      groupQuaternionFlat.push(d.z)
+      groupQuaternionFlat.push(d.w)
+      let q = d.clone()
+      q.invert()
+      eyeQuaternionFlat.push(q.x)
+      eyeQuaternionFlat.push(q.y)
+      eyeQuaternionFlat.push(q.z)
+      eyeQuaternionFlat.push(q.w)
+    })
+    let groupRotationTrack = new THREE.QuaternionKeyframeTrack(`grp_scale.quaternion`, time, groupQuaternionFlat)
+    tracks.push(groupRotationTrack)
+    let eyeLeftRotationTrack = new THREE.QuaternionKeyframeTrack(`eyeLeft.quaternion`, time, eyeQuaternionFlat)
+    let eyeRightRotationTrack = new THREE.QuaternionKeyframeTrack(`eyeRight.quaternion`, time, eyeQuaternionFlat)
+    tracks.push(eyeLeftRotationTrack)
+    tracks.push(eyeRightRotationTrack)
+    const clip = new THREE.AnimationClip('animation', -1, tracks);
+    console.log(clip)
+    return clip
+  }
+  return null
+}
+
+document.getElementById("record").addEventListener("click", () => {
+  if (!recording) {
+    recordedData = []
+    document.getElementById("record").innerText = "Stop"
+    recording = true
+  } else {
+    document.getElementById("record").innerText = "Record"
+    recording = false
+  }
+})
+
+document.getElementById("download").addEventListener("click", () => {
+  let clip = createAnimation()
+  //mixer.clipAction(clip).play()
+  //console.log(head)
+  const gltfExporter = new GLTFExporter()
+  let options = { animations: [clip] }
+  gltfExporter.parse(
+    grpScale,
+    function (result) {
+      if (result instanceof ArrayBuffer) {
+        saveArrayBuffer(result, 'scene.glb');
+      } else {
+        const output = JSON.stringify(result, null, 2);
+        saveString(output, 'scene.gltf');
+      }
+    },
+    options
+  );
+});
+
+//saving 3d files
+const link = document.createElement('a')
+link.style.display = 'none'
+document.body.appendChild(link)
+function save (blob, filename) {
+  link.href = URL.createObjectURL(blob);
+  link.download = filename;
+  link.click();
+}
+
+function saveString (text, filename) {
+  save(new Blob([text], { type: 'text/plain' }), filename);
+}
+
+function saveArrayBuffer (buffer, filename) {
+  save(new Blob([buffer], { type: 'application/octet-stream' }), filename);
+}
+
+getWebcam();
+init();
+tracking();
+
---
+++ vite.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    build: {
+        chunkSizeWarningLimit: 1000
+    },
+    base: "/three-js-website/"
+}
