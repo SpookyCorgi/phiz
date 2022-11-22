5cb017a48046a91b97c1a990d3d0b70fa0b28710
james <b05502067@ntu.edu.tw>
2022-11-22 04:15:38 -0500

    add 3d face test

--- main.js
+++ main.js
@@ -12,54 +12,46 @@ const canvasElement = document.querySelector('.output_canvas');
 const canvasCtx = canvasElement.getContext('2d');
 const threeCanvasElement = document.querySelector('.three_canvas')
 
-//mediapipe
-const faceMesh = new FaceMesh({
+//mediapipe 3d
+const faceMesh3D = new FaceMesh({
     locateFile: (file) => {
         return `https://cdn.jsdelivr.net/npm/@mediapipe/face_mesh/${file}`;
     }
 });
-let camera;
+
+const faceMeshRefine = new FaceMesh({
+    locateFile: (file) => {
+        return `https://cdn.jsdelivr.net/npm/@mediapipe/face_mesh/${file}`;
+    }
+});
+
+
+
+let camera3D
 let width = 0;
 let height = 0;
 
 //three js components
-let dev = true
+let dev = false
 //constants
-const FOV = 15
-const landmarkScaleConstant = 24
-const positionScale = 0.5
-const samplePoint = [10, 454, 234]
+const FOV = 63
 //basic scene component
 let scene, threeCamera, renderer, controls
 //fov magic
-let filmWidth = 0
-let filmHeight = 0
-let landmarkScale
-let faceVerticalLength
-let faceFilmRatio = 0
-let focalLength = 0
-//transform matrix for face
-let invertBaseTransformMatrix = new THREE.Matrix4();
+
 //landmarks
 let spheres = []
-let line
+let faceLandmarks = new THREE.Group()
+let mesh
 let baseNode, topNode, bottomNode
-//video as a three js texture
-let videoTexture
-let projectedObject
-let mask1, mask2
+let refineSpheres = []
+let refineLandmarks = new THREE.Group()
+let clock
+
 function init () {
-    //set three js environment scale
-    landmarkScale = landmarkScaleConstant / height
-
-    //calculate reference distance
-    let t = new THREE.Vector3(positions[samplePoint[1]][0], positions[samplePoint[1]][1], positions[samplePoint[1]][2])
-    let b = new THREE.Vector3(positions[samplePoint[2]][0], positions[samplePoint[2]][1], positions[samplePoint[2]][2])
-    faceVerticalLength = t.distanceTo(b) * positionScale
-
     //set up scene
     scene = new THREE.Scene()
-
+    clock = new THREE.Clock()
     //set up renderer
     renderer = new THREE.WebGLRenderer({
         canvas: threeCanvasElement,
@@ -71,11 +63,7 @@ function init () {
     renderer.setSize(width, height)
 
     //set up camera
-    threeCamera = new THREE.PerspectiveCamera(FOV, width / height, 0.01, 1000);
-    filmWidth = threeCamera.getFilmWidth()
-    filmHeight = threeCamera.getFilmHeight()
-    focalLength = threeCamera.getFocalLength()
-    faceFilmRatio = faceVerticalLength / filmHeight
+    threeCamera = new THREE.PerspectiveCamera(FOV, width / height, 1, 10000);
 
     //development mode
     if (dev) {
@@ -89,7 +77,7 @@ function init () {
         const gridHelper = new THREE.GridHelper(size, divisions);
         scene.add(gridHelper);
     }
-    threeCamera.position.set(0, 0, focalLength)
+    threeCamera.position.set(0, 0, 0)
 
     //add lights
     const directionalLight0 = new THREE.DirectionalLight(0xffffff, 1)
@@ -97,155 +85,132 @@ function init () {
 
     //add landmark spheres
     let points = []
-    for (let i = 0; i < 478; i++) {
+
+    for (let i = 0; i < 468; i++) {
         const geometry = new THREE.SphereGeometry(0.1, 12, 6);
-        let color = new THREE.Color("rgb(0, 0, 0)")
-        const material = new THREE.MeshBasicMaterial({ color: color })
+
+        const material = new THREE.MeshStandardMaterial({ color: 0x000000, opacity: 0.0, transparent: true })
         const sphere = new THREE.Mesh(geometry, material)
-        scene.add(sphere)
+        faceLandmarks.add(sphere)
         spheres.push(sphere)
         points.push(new THREE.Vector3(0, 0, 0))
+
+
+        let refineSphere = new THREE.Mesh(geometry, new THREE.MeshBasicMaterial({ color: 0xffffff }))
+        refineSpheres.push(refineSphere)
+        refineLandmarks.add(refineSphere)
     }
+    scene.add(refineLandmarks)
+    //scene.add(faceLandmarks)
 
-    const lineGeometry = new THREE.BufferGeometry().setFromPoints(points);
-    lineGeometry.setIndex(indices)
-    let lineMaterial = new THREE.MeshBasicMaterial({ color: 0x000000 })
-    lineMaterial.wireframe = true
-    line = new THREE.Mesh(lineGeometry, lineMaterial);
-    scene.add(line);
-
-    //use the nose sphere as base node for all 3d model
-    baseNode = spheres[4]
-    topNode = spheres[10]
-    bottomNode = spheres[152]
+    let buffergeometry = new THREE.BufferGeometry().setFromPoints(points);
+    buffergeometry.setIndex(indices)
+    let bufferMaterial = new THREE.MeshNormalMaterial({ color: new THREE.Color(0xffffff), opacity: 0.9, transparent: true, flatShading: true })
+    mesh = new THREE.Mesh(buffergeometry, bufferMaterial);
+    scene.add(mesh);
 
     //video texture
-    videoTexture = new THREE.VideoTexture(videoElement);
-
-    // instantiate a loader
-    // const manager = new THREE.LoadingManager();
-    // manager.onStart = function (url, itemsLoaded, itemsTotal) {
-    //     console.log('Started loading file: ' + url + '.\nLoaded ' + itemsLoaded + ' of ' + itemsTotal + ' files.');
-    // };
-    // manager.onLoad = function () {
-    //     console.log('Loading complete!');
-    // };
-    // manager.onProgress = function (url, itemsLoaded, itemsTotal) {
-    //     console.log('Loading file: ' + url + '.\nLoaded ' + itemsLoaded + ' of ' + itemsTotal + ' files.');
-    // };
-    // manager.onError = function (url) {
-    //     console.log('There was an error loading ' + url);
-    // };
-
-    // const objLoader = new OBJLoader(manager);
-    // objLoader.load('', function (object) {
-
-    // })
-
-    // const textureLoader = new THREE.TextureLoader(manager);
-    // textureLoader.load(
-    //     '',
-    //     function (texture) {
-    //     }
-    // )
-
-
-    //calculate homogeneous transform matrix from the obj model
-    let p0 = new THREE.Vector3(positions[samplePoint[0]][0] * positionScale, positions[samplePoint[0]][1] * positionScale, positions[samplePoint[0]][2] * positionScale)
-    let p1 = new THREE.Vector3(positions[samplePoint[1]][0] * positionScale, positions[samplePoint[1]][1] * positionScale, positions[samplePoint[1]][2] * positionScale)
-    let p2 = new THREE.Vector3(positions[samplePoint[2]][0] * positionScale, positions[samplePoint[2]][1] * positionScale, positions[samplePoint[2]][2] * positionScale)
-
-    invertBaseTransformMatrix = new THREE.Matrix4()
-    const x = p1.clone().sub(p0).normalize()
-    const y = p2.clone().sub(p0).normalize()
-    const z = new THREE.Vector3().crossVectors(x, y).normalize()
-    const y2 = new THREE.Vector3().crossVectors(x, z).normalize()
-    const z2 = new THREE.Vector3().crossVectors(x, y2).normalize()
-
-    invertBaseTransformMatrix.makeBasis(x, y2, z2)
-    invertBaseTransformMatrix.setPosition(p0.x, p0.y, p0.z)
-    invertBaseTransformMatrix.invert()
+    //videoTexture = new THREE.VideoTexture(videoElement);
 }
 
-function transformLandmarks (landmark) {
-    let obj = {}
-    obj.x = ((landmark.x - 0.5) * width) * landmarkScale
-    obj.y = ((-landmark.y + 0.5) * height) * landmarkScale
-    obj.z = -landmark.z * width * landmarkScale
-    return obj
+let refineResults
+function onResultsRefine (results) {
+
+    if (results.multiFaceLandmarks) {
+        refineResults = results.multiFaceLandmarks[0]
+    }
 }
 
-function getMatrix (landmarks) {
-    //calculate homogeneous transform matrix from the obj model
-    let p0 = new THREE.Vector3(landmarks[samplePoint[0]].x, landmarks[samplePoint[0]].y, landmarks[samplePoint[0]].z)
-    let p1 = new THREE.Vector3(landmarks[samplePoint[1]].x, landmarks[samplePoint[1]].y, landmarks[samplePoint[1]].z)
-    let p2 = new THREE.Vector3(landmarks[samplePoint[2]].x, landmarks[samplePoint[2]].y, landmarks[samplePoint[2]].z)
-    const matrix = new THREE.Matrix4();
-    const x = p1.clone().sub(p0).normalize()
-    const y = p2.clone().sub(p0).normalize()
-    const z = new THREE.Vector3().crossVectors(x, y).normalize()
-    const y2 = new THREE.Vector3().crossVectors(x, z).normalize()
-    const z2 = new THREE.Vector3().crossVectors(x, y2).normalize()
-    matrix.makeBasis(x, y2, z2);
-    matrix.setPosition(p0.x, p0.y, p0.z)
-
-    const result = new THREE.Matrix4()
-    result.multiplyMatrices(matrix, invertBaseTransformMatrix)
-
-    return result
-}
-
+let count = 0
 function onResults (results) {
+    //console.log(1 / clock.getDelta())
     canvasCtx.save();
     canvasCtx.clearRect(0, 0, width, height);
     canvasCtx.drawImage(
         results.image, 0, 0, width, height);
-    if (results.multiFaceLandmarks) {
-        for (const landmarks of results.multiFaceLandmarks) {
-            //console.log(FACEMESH_TESSELATION)
-            // drawConnectors(canvasCtx, landmarks, FACEMESH_TESSELATION,
-            //     { color: '#C0C0C070', lineWidth: 1 });
-            // drawConnectors(canvasCtx, landmarks, FACEMESH_RIGHT_EYE, { color: '#FF3030' });
-            // drawConnectors(canvasCtx, landmarks, FACEMESH_RIGHT_EYEBROW, { color: '#FF3030' });
-            // drawConnectors(canvasCtx, landmarks, FACEMESH_RIGHT_IRIS, { color: '#FF3030' });
-            // drawConnectors(canvasCtx, landmarks, FACEMESH_LEFT_EYE, { color: '#30FF30' });
-            // drawConnectors(canvasCtx, landmarks, FACEMESH_LEFT_EYEBROW, { color: '#30FF30' });
-            // drawConnectors(canvasCtx, landmarks, FACEMESH_LEFT_IRIS, { color: '#30FF30' });
-            // drawConnectors(canvasCtx, landmarks, FACEMESH_FACE_OVAL, { color: '#E0E0E0' });
-            // drawConnectors(canvasCtx, landmarks, FACEMESH_LIPS, { color: '#E0E0E0' });
 
-            const top = transformLandmarks(landmarks[samplePoint[1]])
-            const topLandmark = new THREE.Vector3(top.x, top.y, top.z)
-            const bottom = transformLandmarks(landmarks[samplePoint[2]])
-            const bottomLandmark = new THREE.Vector3(bottom.x, bottom.y, bottom.z)
-            const dis = topLandmark.distanceTo(bottomLandmark);
-            const scale = faceVerticalLength / dis
-            const deltaZ = focalLength - (scale * focalLength)
+    if (results.multiFaceGeometry.length && results.multiFaceLandmarks.length) {
+        // for (const landmarks of results.multiFaceLandmarks) {
+        //     drawConnectors(canvasCtx, landmarks, FACEMESH_TESSELATION,
+        //         { color: '#C0C0C070', lineWidth: 1 });
+        //     drawConnectors(canvasCtx, landmarks, FACEMESH_RIGHT_EYE, { color: '#FF3030' });
+        //     drawConnectors(canvasCtx, landmarks, FACEMESH_RIGHT_EYEBROW, { color: '#FF3030' });
+        //     drawConnectors(canvasCtx, landmarks, FACEMESH_RIGHT_IRIS, { color: '#FF3030' });
+        //     drawConnectors(canvasCtx, landmarks, FACEMESH_LEFT_EYE, { color: '#30FF30' });
+        //     drawConnectors(canvasCtx, landmarks, FACEMESH_LEFT_EYEBROW, { color: '#30FF30' });
+        //     drawConnectors(canvasCtx, landmarks, FACEMESH_LEFT_IRIS, { color: '#30FF30' });
+        //     drawConnectors(canvasCtx, landmarks, FACEMESH_FACE_OVAL, { color: '#E0E0E0' });
+        //     drawConnectors(canvasCtx, landmarks, FACEMESH_LIPS, { color: '#E0E0E0' });
+        // }
 
-            let newLandmarks = []
-            let mask1Vertices = []
-            let mask2Vertices = []
-            landmarks.forEach((mark, i) => {
-                let l = transformLandmarks(mark)
-                let tempMark = { x: l.x * scale, y: l.y * scale, z: l.z + deltaZ }
-                newLandmarks.push(tempMark)
-                mask1Vertices.push(l.x * scale)
-                mask1Vertices.push(l.y * scale)
-                mask1Vertices.push(l.z + deltaZ)
-                spheres[i].position.set(tempMark.x, tempMark.y, tempMark.z)
-                line.geometry.attributes.position.setXYZ(i, spheres[i].position.x, spheres[i].position.y, spheres[i].position.z)
-            })
-
-            line.geometry.attributes.position.needsUpdate = true;
-
-            let m = getMatrix(newLandmarks)
-            let q = new THREE.Quaternion()
-            q.setFromRotationMatrix(m)
-            baseNode.quaternion.set(q.x, q.y, q.z, q.w)
+        let transformMatrix = results.multiFaceGeometry[0].getPoseTransformMatrix()
+        let m = matrixDataToMatrix(transformMatrix)
+        let mArr = []
+        for (let i = 0; i < 4; i++) {
+            for (let j = 0; j < 4; j++) {
+                mArr.push(m[j][i])
+            }
         }
+
+        const matrix = new THREE.Matrix4().fromArray(mArr)
+        let v = results.multiFaceGeometry[0].getMesh().getVertexBufferList()
+
+        for (let i = 0; i < v.length / 5; i++) {
+            spheres[i].position.set(v[5 * i], v[5 * i + 1], v[5 * i + 2])
+        }
+
+
+        faceLandmarks.rotation.setFromRotationMatrix(matrix)
+        faceLandmarks.position.setFromMatrixPosition(matrix)
+        faceLandmarks.scale.setFromMatrixScale(matrix)
+        let landmarks2D = results.multiFaceLandmarks[0]
+        let aArr = []
+        let bArr = []
+        for (let i = 0; i < spheres.length; i++) {
+            let v = new THREE.Vector3()
+            spheres[i].getWorldPosition(v)
+            let v1 = new THREE.Vector3()
+            spheres[(i + 1) % spheres.length].getWorldPosition(v1)
+
+            let diff = landmarks2D[(i + 1) % spheres.length].z - landmarks2D[i].z
+            if (diff > 0.01) {
+                let a = (v1.z - v.z) / (landmarks2D[(i + 1) % spheres.length].z - landmarks2D[i].z)
+                let b = v.z - landmarks2D[i].z * a
+
+                aArr.push(a)
+                bArr.push(b)
+            }
+        }
+
+        let iMatrix = matrix.clone().invert()
+        const average = array => array.reduce((a, b) => a + b) / array.length;
+        if (aArr.length && bArr.length && refineResults) {
+            let a = average(aArr)
+            let b = average(bArr)
+            for (let i = 0; i < spheres.length; i++) {
+                let z = refineResults[i].z * a + b
+                let x = (0.5 - refineResults[i].x) * (Math.tan(FOV / 2 / 180 * Math.PI) * 2) / height * width * z
+                let y = (refineResults[i].y - 0.5) * (Math.tan(FOV / 2 / 180 * Math.PI) * 2) * z
+                refineSpheres[i].position.set(x, y, z)
+                refineSpheres[i].applyMatrix4(iMatrix)
+                mesh.geometry.attributes.position.setXYZ(i, refineSpheres[i].position.x, refineSpheres[i].position.y, refineSpheres[i].position.z)
+                mesh.geometry.attributes.position.needsUpdate = true;
+                mesh.geometry.computeVertexNormals()
+            }
+            refineLandmarks.rotation.setFromRotationMatrix(matrix)
+            refineLandmarks.position.setFromMatrixPosition(matrix)
+            refineLandmarks.scale.setFromMatrixScale(matrix)
+            mesh.rotation.setFromRotationMatrix(matrix)
+            mesh.position.setFromMatrixPosition(matrix)
+            mesh.scale.setFromMatrixScale(matrix)
+        }
+
+        // if (count < 48) {
+        //     refineLandmarks.rotateY(Math.PI / 24 * count)
+        //     mesh.rotateY(Math.PI / 24 * count)
+        //     count++
+        // }
     }
-    //project video texture
-    //project(projectedObject)
 
     if (dev) {
         controls.update()
@@ -255,27 +220,40 @@ function onResults (results) {
 }
 
 function setUpMediaPipe () {
-    faceMesh.setOptions({
+    faceMesh3D.setOptions({
         maxNumFaces: 1,
         refineLandmarks: false,
         minDetectionConfidence: 0.5,
-        minTrackingConfidence: 0.5
+        minTrackingConfidence: 0.5,
+        enableFaceGeometry: true,
+        selfieMode: true
     });
 
-    camera = new Camera(videoElement, {
+    faceMeshRefine.setOptions({
+        maxNumFaces: 1,
+        refineLandmarks: true,
+        minDetectionConfidence: 0.5,
+        minTrackingConfidence: 0.5,
+        enableFaceGeometry: false,
+        selfieMode: true
+    });
+
+    camera3D = new Camera(videoElement, {
         onFrame: async () => {
-            await faceMesh.send({ image: videoElement });
+            await faceMesh3D.send({ image: videoElement });
+            await faceMeshRefine.send({ image: videoElement });
         },
     });
-    camera.start()
+    camera3D.start()
 
-    const aspect = camera.h.height / camera.h.width;
+
+    const aspect = camera3D.h.height / camera3D.h.width;
     if (window.innerWidth > window.innerHeight) {
-        height = window.innerHeight;
-        width = height / aspect;
-    } else {
-        width = window.innerWidth;
+        width = window.innerWidth / 2;
         height = width * aspect;
+    } else {
+        height = window.innerHeight / 2;
+        width = height / aspect;
     }
     canvasElement.width = width
     canvasElement.height = height
@@ -285,13 +263,13 @@ function setUpMediaPipe () {
 
 
 function resizeCanvas () {
-    const aspect = camera.h.height / camera.h.width;
+    const aspect = camera3D.h.height / camera3D.h.width;
     if (window.innerWidth > window.innerHeight) {
-        height = window.innerHeight;
-        width = height / aspect;
-    } else {
-        width = window.innerWidth;
+        width = window.innerWidth / 2;
         height = width * aspect;
+    } else {
+        height = window.innerHeight / 2;
+        width = height / aspect;
     }
     canvasElement.width = width
     canvasElement.height = height
@@ -300,12 +278,11 @@ function resizeCanvas () {
     threeCamera.aspect = width / height
     threeCamera.updateProjectionMatrix()
     renderer.setSize(width, height)
-
-    landmarkScale = landmarkScaleConstant / height
 }
 
 window.addEventListener('resize', resizeCanvas);
 
 setUpMediaPipe()
 init()
-faceMesh.onResults(onResults);
+faceMesh3D.onResults(onResults);
+faceMeshRefine.onResults(onResultsRefine);
