f39d935410470759203d65bfc726f4386327ca1a
James Lee <james.lee@nyu.edu>
2023-01-04 06:10:50 -0500

    add vercel support

--- site/svelte.config.js
+++ site/svelte.config.js
@@ -1,6 +1,7 @@
 //import adapter from '@sveltejs/adapter-node';
 //import adapter from '@sveltejs/adapter-auto';
-import adapter from '@sveltejs/adapter-static';
+//import adapter from '@sveltejs/adapter-static';
+import vercel from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
 
@@ -11,9 +12,19 @@ const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
-		adapter: adapter({
-			pages: "dist",
-			assets: "dist"
+		adapter: vercel({
+			// if true, will deploy the app using edge functions
+			// (https://vercel.com/docs/concepts/functions/edge-functions)
+			// rather than serverless functions
+			edge: false,
+
+			// an array of dependencies that esbuild should treat
+			// as external when bundling functions
+			external: [],
+
+			// if true, will split your app into multiple functions
+			// instead of creating a single one for the entire app
+			split: false
 		}),
 		paths: {
 			// change below to your repo name
