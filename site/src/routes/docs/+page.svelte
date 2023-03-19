b895cd5a8fea02483bb4a5331165c4d9e80c160c
James Lee <james.lee@nyu.edu>
2023-03-18 22:59:35 -0400

    fix doc

--- site/src/routes/docs/+page.svelte
+++ site/src/routes/docs/+page.svelte
@@ -8,62 +8,7 @@
 </script>
 
 <main>
-	<div class="markdown-body container-lg">
-		<h2 data-sourcepos="17:1-17:18" dir="auto">
-			<a id="user-content-getting-started" class="anchor" aria-hidden="true" href="#getting-started"
-				><svg
-					class="octicon octicon-link"
-					viewBox="0 0 16 16"
-					version="1.1"
-					width="16"
-					height="16"
-					aria-hidden="true"
-					><path
-						d="m7.775 3.275 1.25-1.25a3.5 3.5 0 1 1 4.95 4.95l-2.5 2.5a3.5 3.5 0 0 1-4.95 0 .751.751 0 0 1 .018-1.042.751.751 0 0 1 1.042-.018 1.998 1.998 0 0 0 2.83 0l2.5-2.5a2.002 2.002 0 0 0-2.83-2.83l-1.25 1.25a.751.751 0 0 1-1.042-.018.751.751 0 0 1-.018-1.042Zm-4.69 9.64a1.998 1.998 0 0 0 2.83 0l1.25-1.25a.751.751 0 0 1 1.042.018.751.751 0 0 1 .018 1.042l-1.25 1.25a3.5 3.5 0 1 1-4.95-4.95l2.5-2.5a3.5 3.5 0 0 1 4.95 0 .751.751 0 0 1-.018 1.042.751.751 0 0 1-1.042.018 1.998 1.998 0 0 0-2.83 0l-2.5 2.5a1.998 1.998 0 0 0 0 2.83Z"
-					/></svg
-				></a
-			>Getting started
-		</h2>
-		<p data-sourcepos="18:1-18:133" dir="auto">
-			<strong
-				>Important! The browser page needs to be active when recording. If you closed your phone,
-				refresh the page to generate a new code.</strong
-			>
-		</p>
-		<ol data-sourcepos="20:1-22:0" dir="auto">
-			<li data-sourcepos="20:1-20:97">
-				Go to <a href="https://phizmocap.dev/blendshapes" rel="nofollow"
-					>phizmocap.dev/blendshapes</a
-				> on your recording device.
-			</li>
-			<li data-sourcepos="21:1-22:0">Allow camera access.</li>
-		</ol>
-		<p data-sourcepos="23:1-23:59" dir="auto">
-			<strong>For recording and controlling avatar on the same device</strong>
-		</p>
-		<ol start="3" data-sourcepos="25:1-26:0" dir="auto">
-			<li data-sourcepos="25:1-26:0">Select local mode.</li>
-		</ol>
-		<p data-sourcepos="27:1-27:60" dir="auto">
-			<strong>For recording and controlling avatar on seperate devices</strong>
-		</p>
-		<ol start="3" data-sourcepos="29:1-32:0" dir="auto">
-			<li data-sourcepos="29:1-30:0">
-				<p data-sourcepos="29:4-29:160" dir="auto">
-					Select remote mode. Copy the 9 character code (eg, p-AwCodE). Go to <a
-						href="https://www.phizmocap.dev/data"
-						rel="nofollow">phizmocap.dev/data</a
-					> on your computer and paste the code.
-				</p>
-			</li>
-			<li data-sourcepos="31:1-32:0">
-				<p data-sourcepos="31:4-31:118" dir="auto">
-					Enter the correct host and post based on plugin's setup. Press connect. See plugins
-					section below for more details.
-				</p>
-			</li>
-		</ol>
-	</div>
+	<h1>Getting started</h1>
 </main>
 
 <style>
